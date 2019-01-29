9b4ac3c07bc28bf9bcfd75c0014b564bd6e880de
jamespagedev <jamespagedev@outlook.com>
2019-01-29 00:36:04 -0700

    added test for fail()

--- Enhancer.js
+++ Enhancer.js
@@ -1,4 +1,6 @@
+const itemTypes = ['weapon', 'armor'];
 const enhancementLvls = [
+  0,
   1,
   2,
   3,
@@ -20,13 +22,75 @@ const enhancementLvls = [
   'TET',
   'PEN'
 ];
+const armorSafeLvl = enhancementLvls[5];
+const weaponSafeLvl = enhancementLvls[7];
 
 const success = item => {
-  item.enhancement = item.enhancement + 1;
-  item.name = `[+${item.enhancement}] ${item.origName}`;
+  // Precondition1 - If the item's enhancement is 14 or lower, the item cannot be enhanced if the durability is below 25.
+  // Precondition2 - If the item's enhancement is 15 or higher, the item cannot be enhanced if the durability is below 10.
+
+  if (item.enhancement !== enhancementLvls[enhancementLvls.length - 1]) {
+    item.enhancement =
+      enhancementLvls[enhancementLvls.indexOf(item.enhancement) + 1];
+  }
+
+  // After +15 the display for the enhancing level must result in the string names listed in the enhancementLvls[]
+  if (enhancementLvls.indexOf(item.enhancement) < enhancementLvls.indexOf(15)) {
+    // less than +15
+    item.name = `[+${item.enhancement}] ${item.origName}`;
+  } else if (
+    // less than 'PEN'
+    enhancementLvls.indexOf(item.enhancement) < enhancementLvls.indexOf('PEN')
+  ) {
+    item.name = `[${item.enhancement}] ${item.origName}`;
+  }
+
+  console.log(item);
+  return item;
+};
+
+const fail = item => {
+  // Precondition1 - If the item's current enhancement is 14 or lower, item durability must be >= 25.
+  // Precondition2 - If the item's current enhancement is 15 or higher, item durability must be >= 10.
+
+  // weapon
+  if (item.type === itemTypes[0]) {
+    if (item.enhancement === weaponSafeLvl) {
+      item.durability = Math.abs(item.durability - 5);
+    } else if (
+      enhancementLvls.indexOf(item.enhancement) < enhancementLvls.indexOf('DUO')
+    ) {
+      item.durability = Math.abs(item.durability - 5);
+    } else if (
+      enhancementLvls.indexOf(item.enhancement) < enhancementLvls.indexOf('PEN')
+    ) {
+      item.enhancement =
+        enhancementLvls[enhancementLvls.indexOf(item.durability) - 1];
+      item.durability = Math.abs(item.durability - 10);
+    }
+  }
+
+  // armor
+  if (item.type === itemTypes[1]) {
+    if (item.enhancement === armorSafeLvl) {
+      item.durability = Math.abs(item.durability - 5);
+    } else if (
+      enhancementLvls.indexOf(item.enhancement) < enhancementLvls.indexOf('DUO')
+    ) {
+      item.durability = Math.abs(item.durability - 5);
+    } else if (
+      enhancementLvls.indexOf(item.enhancement) < enhancementLvls.indexOf('PEN')
+    ) {
+      item.enhancement =
+        enhancementLvls[enhancementLvls.indexOf(item.durability) - 1];
+      item.durability = Math.abs(item.durability - 10);
+    }
+  }
+
   return item;
 };
 
 module.exports = {
-  success
+  success,
+  fail
 };
--- __tests__/Enhancer.js
+++ __tests__/Enhancer.js
@@ -4,15 +4,32 @@ const enhancer = require('../Enhancer');
 // enhancer test suite
 describe('enhancer test suite', () => {
   describe('success(item) test(s)', () => {
-    test('enhancing success increases enhancing level', () => {
+    test('Item going +0 to +1 changes enhancement and adds detail to name', () => {
       // Setup
-      const weapon = createItem('weapon', 'sword');
-      const expected = Object.assign({}, weapon);
-      expected.enhancement = weapon.enhancement + 1;
-      expected.name = `[+${weapon.enhancement + 1}] ${weapon.origName}`;
+      const weapon1 = createItem('weapon', 'sword');
+      const expected = Object.assign({}, weapon1);
+      expected.enhancement = 1;
+      expected.name = `[+1] ${weapon1.origName}`;
 
       // act - execute SUT (System Under Test)
-      const result = enhancer.success(weapon);
+      const result = enhancer.success(weapon1);
+
+      // assert
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('fail(item) test(s)', () => {
+    test('enhancing "[+8] item" fail decrease durability by 5, ', () => {
+      // Setup
+      const weapon2 = createItem('weapon', 'sword');
+      weapon2.enhancement = 8;
+      weapon2.name = '[+8] Sword';
+      const expected = Object.assign({}, weapon2);
+      expected.durability = 95;
+
+      // act - execute SUT (System Under Test)
+      const result = enhancer.fail(weapon2);
 
       // assert
       expect(result).toEqual(expected);
